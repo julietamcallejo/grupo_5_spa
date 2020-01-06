489fb52536a4528f8470272ef616eadb7ff43ecb
Hacho55 <horacio.vfx@gmail.com>
2020-01-06 16:34:38 -0300

    Nuevos enlaces en header

--- src/routes/main.js
+++ src/routes/main.js
@@ -15,7 +15,6 @@ router.get('/productDetail/:idProduct', productController.detailId);
 router.get('/productCart', mainController.productCart);
 router.get('/register', mainController.register);
 router.get('/productAdd', mainController.productAdd);
-router.get('/navBar', mainController.test);
-router.get('/navBar2', mainController.test2);
+
 
 module.exports = router;
