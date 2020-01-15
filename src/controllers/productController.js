edaaf5710a2116a14f2b17da27fe66dc036925d0
Caterina Znidarsic <caterina.znidarsic@turner.com>
2020-01-15 17:56:52 -0300

    Calendario funcionando. Ciertos fixes minimos.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,16 +4,15 @@ const path = require('path');
 const pathProductos = path.join(__dirname, '../data/productos.json'); 
 //**** Helpers ****//
 
+
 function traerProductos () {
     let productFileContent = fs.readFileSync(pathProductos, 'utf-8');
     let productArray;
-
     if (productFileContent == '') {
         productArray = [];
     }else{
         productArray = JSON.parse(productFileContent);
     };
-
     return productArray;
 };
 
@@ -32,18 +31,13 @@ function guardarProducto (datoProducto) {
     fs.writeFileSync(pathProductos, JSON.stringify(productos, null, ''));
 };
 
-
-
 const detalleProductos = traerProductos();
-
 
 const productController = {
     list: (req, res) => {
         res.render('productList', {detalleProducto: detalleProductos});
     },
-
     detailId: (req, res) => {
-        
         let id = req.params.idProduct;
         res.render('productDetail', {detalleProducto: detalleProductos[id]});
     },
