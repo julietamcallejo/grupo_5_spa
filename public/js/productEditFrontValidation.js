dd96aa0d5d3872e8fe612edcc8970e259d7bc397
Julieta Callejo <callejo.jm@gmail.com>
2020-04-07 23:09:09 -0300

    creación del archivo productEditFrontValidation.js y conexión con productEdit.ejs

---
+++ public/js/productEditFrontValidation.js
@@ -0,0 +1,73 @@
+let form = document.querySelector('#productEditForm');
+let submitButton = document.querySelector('.btn');
+
+let formElements = Array.from(form.elements);
+formElements.pop();
+
+//Objeto de errores
+let inputsErrors = {};
+
+formElements.forEach(function (input) {
+    input.addEventListener('blur', function () {
+        let inputValue = this.value;
+        //validador
+        if (validator.isEmpty(inputValue)) {
+            this.classList.add('is-invalid');
+            this.classList.remove('is-valid');
+            this.nextElementSibling.innerHTML = `El campo <b>${this.dataset.name}</b> es obligatorio`;
+            inputsErrors[this.name] = true;
+        } else if (!validator.isLength(inputValue, { min: 3, max: undefined })) {
+
+            this.classList.add('is-invalid');
+            this.classList.remove('is-valid');
+            this.nextElementSibling.innerHTML = `El campo <b>${this.dataset.name}</b> debe contener al menos 3 caracteres.`;
+            inputsErrors[this.name] = true;
+
+        } else {
+            this.classList.remove('is-invalid');
+            this.classList.add('is-valid');
+            this.nextElementSibling.innerHTML = '';
+            delete inputsErrors[this.name];
+        }
+
+    });
+
+    if (input.name == 'photo') {
+        input.addEventListener('change', function () {
+            let fileExtension = this.value.split('.').pop();
+            let acceptedExtensions = ['jpg', 'jpeg', 'png', 'webm', 'svg'];
+            if (!acceptedExtensions.includes(fileExtension)) {
+                this.classList.add('is-invalid');
+                this.classList.remove('is-valid');
+                this.nextElementSibling.innerHTML = `Formato de imagen no aceptada. Los formatos de imagen aceptados son: ${acceptedExtensions}`;
+                inputsErrors[this.name] = true;
+            } else if (validator.isEmpty(inputValue)) {
+                this.classList.add('is-invalid');
+                this.classList.remove('is-valid');
+                this.nextElementSibling.innerHTML = `El campo <b>${this.dataset.name}</b> es obligatorio`;
+                inputsErrors[this.name] = true;
+            } else {
+                this.classList.remove('is-invalid');
+                this.classList.add('is-valid');
+                this.nextElementSibling.innerHTML = '';
+                delete inputsErrors[this.name];
+            }
+        });
+    }
+});
+
+form.addEventListener('submit', function (e) {
+    formElements.forEach(function (input) {
+        if (validator.isEmpty(input.value)) {
+            inputsErrors[input.name] = true;
+            input.classList.add('is-invalid');
+            input.nextElementSibling.innerHTML = 'Campo obligatorio';
+        }
+    });
+
+    console.table(inputsErrors);
+
+    if (Object.keys(inputsErrors).length > 0) {
+        e.preventDefault();
+    }
+});
