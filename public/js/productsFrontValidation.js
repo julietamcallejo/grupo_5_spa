f35b0e7dbaad3e38c29cca30fd9207e319ed3bad
Julieta Callejo <callejo.jm@gmail.com>
2020-04-07 22:38:06 -0300

    Limpieza de archivo productsFrontValidation

--- public/js/productsFrontValidation.js
+++ public/js/productsFrontValidation.js
@@ -4,7 +4,6 @@ let submitButton = document.querySelector('.btn');
 let formElements = Array.from(form.elements);
 formElements.pop();
 formElements.pop();
-//splice(-1,-2);
 
 //Objeto de errores
 let inputsErrors = {};
@@ -35,63 +34,41 @@ formElements.forEach(function (input) {
     });
 
     if (input.name == 'photo') {
-		input.addEventListener('change', function () {
-			let fileExtension = this.value.split('.').pop();
-			let acceptedExtensions = ['jpg', 'jpeg', 'png', 'webm', 'svg'];
-			if(!acceptedExtensions.includes(fileExtension)) {
-				this.classList.add('is-invalid');
-				this.classList.remove('is-valid');
-				this.nextElementSibling.innerHTML = `Formato de imagen no aceptada. Los formatos de imagen aceptados son: ${acceptedExtensions}`;
-				inputsWithErrors[this.name] = true;
-			} else {
-				this.classList.remove('is-invalid');
-				this.classList.add('is-valid');
-				this.nextElementSibling.innerHTML = '';
-				delete inputsWithErrors[this.name];
-			}
-		});
-	}
+        input.addEventListener('change', function () {
+            let fileExtension = this.value.split('.').pop();
+            let acceptedExtensions = ['jpg', 'jpeg', 'png', 'webm', 'svg'];
+            if (!acceptedExtensions.includes(fileExtension)) {
+                this.classList.add('is-invalid');
+                this.classList.remove('is-valid');
+                this.nextElementSibling.innerHTML = `Formato de imagen no aceptada. Los formatos de imagen aceptados son: ${acceptedExtensions}`;
+                inputsErrors[this.name] = true;
+            } else if (validator.isEmpty(inputValue)) {
+                this.classList.add('is-invalid');
+                this.classList.remove('is-valid');
+                this.nextElementSibling.innerHTML = `El campo <b>${this.dataset.name}</b> es obligatorio`;
+                inputsErrors[this.name] = true;
+            } else {
+                this.classList.remove('is-invalid');
+                this.classList.add('is-valid');
+                this.nextElementSibling.innerHTML = '';
+                delete inputsErrors[this.name];
+            }
+        });
+    }
 });
 
-/*
-window.addEventListener("load", function(){
-    let formulario = document.querySelector("form.reservation");
-
-    formulario.addEventListener("submit", function(e) {
-        let errores = [];
-
-        let campoNombre = document.querySelector("input.name");
-
-        if (campoNombre.value == "") {
-            errores.push("El campo de nombre tiene que estar completo");
-        } else if (campoNombre.value.length < 3) {
-            errores.push("El campo de nombre debe tener al menos 3 caracteres");
+form.addEventListener('submit', function (e) {
+    formElements.forEach(function (input) {
+        if (validator.isEmpty(input.value)) {
+            inputsErrors[input.name] = true;
+            input.classList.add('is-invalid');
+            input.nextElementSibling.innerHTML = 'Campo obligatorio';
         }
+    });
 
-        let campoMensaje = document.querySelector("input.message");
+    console.table(inputsErrors);
 
-        if (campoMensaje.value == "") {
-            errores.push("El campo de mensaje tiene que estar completo");
-        }
-
-        let campoFecha = document.querySelector("input.message");
-
-        if (campoFecha.value == "") {
-            errores.push("El campo de fecha tiene que estar completo");
-        }
-
-        let campoPersonas = document.querySelector("select.people");
-
-        if (errores.length > 0) {
-            e.preventDefault();
-
-        let ulErrores = document.querySelector(div.errores.ul");
-
-        for (let i = 0; i < errores.length; i++) {
-            ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
-        }}
-
-
-    })
-})
-*/
+    if (Object.keys(inputsErrors).length > 0) {
+        e.preventDefault();
+    }
+});
